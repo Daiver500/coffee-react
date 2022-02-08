acafa2b8ab31ada5b8ba576662d3b72b0c922ebd
Aleksei <60549770+Daiver500@users.noreply.github.com>
2022-02-08 20:39:24 +0300

    Update main.js

--- coffee/src/components/main/main.js
+++ coffee/src/components/main/main.js
@@ -2,14 +2,16 @@ import { Component, Fragment } from 'react';
 import { NavLink } from "react-router-dom";
 
 class Main extends Component {
-  
-    
+    constructor (props) {                            // для передачи props в классовый компонент используется constructor      
+        super(props)
+    }  
     render () {
+        const {img} = this.props
         return(
-            <Fragment>
+            <div>
               <h1>Hello world!!!</h1>
               <button> <NavLink to="../Catalog">Press</NavLink></button>
-            </Fragment>
+            </div>
         )
     }
 }
